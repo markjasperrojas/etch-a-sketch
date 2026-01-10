4e75d5c602352204f9590b5d39b74a9dbec43b28
markjasperrojas <markjasperrojas1@gmail.com>
2026-01-10 21:27:33 +0800

    Finish the logic

---
+++ script.js
@@ -0,0 +1,120 @@
+const sketchPad = document.querySelector(".sketch-pad");
+const pixelSlider = document.getElementById("pixel-slider");
+const gridSize = document.getElementById("grid-size");
+const color = document.getElementById("color");
+const penBtn = document.getElementById("pen");
+const eraserBtn = document.getElementById("eraser");
+const rainbowBtn = document.getElementById("rainbow");
+const clearBtn = document.getElementById("clear");
+const gridLineBtn = document.getElementById("grid-line");
+
+let isPenMode = true;
+let isEraserMode = false;
+let isRainbowMode = false;
+
+pixelSlider.addEventListener("input", () => {
+  gridSize.textContent = `${pixelSlider.value} x ${pixelSlider.value}`;
+});
+
+pixelSlider.addEventListener("click", () => {
+  sketchPad.innerHTML = "";
+  createGrid();
+});
+
+penBtn.addEventListener("click", () => {
+  isPenMode = true;
+  isEraserMode = false;
+  isRainbowMode = false;
+});
+
+eraserBtn.addEventListener("click", () => {
+  isPenMode = false;
+  isEraserMode = true;
+  isRainbowMode = false;
+});
+
+rainbowBtn.addEventListener("click", () => {
+  isPenMode = false;
+  isEraserMode = false;
+  isRainbowMode = true;
+});
+
+clearBtn.addEventListener("click", () => {
+  sketchPad.innerHTML = "";
+  createGrid();
+});
+
+createGrid();
+
+function createGrid() {
+  let isToggleGridLines = true;
+  let isMouseDown = false;
+
+  for (let i = 0; i < pixelSlider.value; i++) {
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+    sketchPad.appendChild(gridContainer);
+
+    for (let j = 0; j < pixelSlider.value; j++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid");
+
+      grid.addEventListener("mousedown", () => {
+        isMouseDown = true;
+        if (isPenMode) {
+          grid.style.backgroundColor = `${color.value}`;
+        } else if (isEraserMode) {
+          grid.style.backgroundColor = "#ffffff";
+        } else {
+          grid.style.backgroundColor = `rgba(${randomNumber()}, ${randomNumber()}, ${randomNumber()}, ${randomNumberDecimal()})`;
+        }
+      });
+
+      grid.addEventListener("mouseover", () => {
+        if (isPenMode && isMouseDown) {
+          grid.style.backgroundColor = `${color.value}`;
+        } else if (isEraserMode && isMouseDown) {
+          grid.style.backgroundColor = "#ffffff";
+        } else if (isRainbowMode && isMouseDown) {
+          grid.style.backgroundColor = `rgba(${randomNumber()}, ${randomNumber()}, ${randomNumber()}, ${randomNumberDecimal()})`;
+        }
+      });
+
+      grid.addEventListener("mouseup", () => {
+        isMouseDown = false;
+      });
+
+      gridContainer.appendChild(grid);
+
+      if (i === pixelSlider.value - 1 && j === pixelSlider.value - 1) {
+        const gridLine = document.querySelectorAll(".grid");
+        gridLine.forEach((grid) => {
+          grid.classList.add("grid-border");
+        });
+        gridLineBtn.addEventListener("click", () => {
+          if (isToggleGridLines) {
+            isToggleGridLines = false;
+            gridLine.forEach((grid) => {
+              grid.classList.remove("grid-border");
+            });
+          } else {
+            isToggleGridLines = true;
+            gridLine.forEach((grid) => {
+              grid.classList.add("grid-border");
+            });
+          }
+        });
+      }
+    }
+  }
+
+  return;
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 255 + 1);
+}
+
+function randomNumberDecimal() {
+  return Math.round(Math.random() * 10) / 10;
+}
